93fd56e9f52f26a7a4de85e80d076d1bc2ef3460
Joe King <joe.k659@gmail.com>
2021-11-03 13:42:13 -0500

    first commit

---
+++ src/pages/Tab1.tsx
@@ -0,0 +1,25 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Tab1.css';
+
+const Tab1: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Personal</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Personal</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <ExploreContainer name="Personal" />
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Tab1;
